3fcc12bc5cd751ad9c63bf55927a31f0de6ca72c
lbenie <lbenie@coveo.com>
2016-09-08 17:02:19 -0400

    fix(rules): remove no-mixed-requires
    unnecessary rule

--- index.js
+++ index.js
@@ -152,9 +152,7 @@ module.exports = {
     'callback-return': 2,
     'global-require': 2,
     'handle-callback-err': 2,
-    'no-mixed-requires': [2, {
-      allowCall: true
-    }],
+    'no-mixed-requires': 0,
     'no-new-require': 2,
     'no-path-concat': 0,
     'no-process-env': 0,
