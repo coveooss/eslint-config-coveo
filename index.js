1ab97e726e90047e63b6b45b7fd4375080d0b454
lbenie <lbenie@coveo.com>
2016-09-08 10:32:11 -0400

    fix(rule): arrow-body-style
    unneeded rule

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ module.exports = {
     'wrap-regex': 0,
 
     // ECMAScript 6
-    'arrow-body-style': 2,
+    'arrow-body-style': 0,
     'arrow-parens': [2, 'as-needed'],
     'arrow-spacing': [2, {
       before: true,
