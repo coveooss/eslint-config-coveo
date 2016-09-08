e58c426b33f0af5a96d5544172eaa77b36659e6a
lbenie <lbenie@coveo.com>
2016-09-08 10:00:42 -0400

    refactor(ava): refactor unit test
    refactor

--- test/test.js
+++ test/test.js
@@ -1,46 +1,44 @@
-'use strict';
-
-var test = require('ava');
-var isPlainObj = require('is-plain-obj');
-var tempWrite = require('temp-write');
-var eslint = require('eslint');
-var conf = require('../');
+import test from 'ava';
+import isPlainObj from 'is-plain-obj';
+import tempWrite from 'temp-write';
+import eslint from 'eslint';
+import conf from '../';
 
 /**
  * lint - Lints code using `Eslint`
  *
  * @param  {String} str  Text to lint
- * @param  {Object} conf Configuration file
+ * @param  {Object} configuration Configuration file
  * @return {Object[]}    Eslint errors
  */
-function lint(str, conf) {
-  var linter = new eslint.CLIEngine({
+const lint = (str, configuration) => {
+  const linter = new eslint.CLIEngine({
     useEslintrc: false,
-    configFile: tempWrite.sync(JSON.stringify(conf))
+    configFile: tempWrite.sync(JSON.stringify(configuration))
   });
 
   return linter.executeOnText(str).results[0].messages;
-}
+};
 
 /**
  * `Eslint` test
  */
-test('It should throw 7 errors', function(t) {
-  var errors = lint("'use strict'\nvar foo = function () {};\nfoo();\n", conf);
+test('It should throw errors', assert => {
+  const errors = lint(`'use strict'\nvar foo = function () {};\nfoo();\n`, conf);
 
-  t.is(errors[0].ruleId, 'class-method-use-this');
-  t.is(errors[1].ruleId, 'symbol-description');
-  t.is(errors[2].ruleId, 'semi');
-  t.is(errors[3].ruleId, 'newline-after-var');
-  t.is(errors[4].ruleId, 'no-var');
-  t.is(errors[5].ruleId, 'no-implicit-globals');
-  t.is(errors[6].ruleId, 'no-empty-function');
+  assert.is(errors[0].ruleId, 'strict');
+  assert.is(errors[1].ruleId, 'class-methods-use-this');
+  assert.is(errors[2].ruleId, 'symbol-description');
+  assert.is(errors[3].ruleId, 'semi');
+  assert.is(errors[4].ruleId, 'newline-after-var');
+  assert.is(errors[5].ruleId, 'no-var');
+  assert.is(errors[6].ruleId, 'no-empty-function');
 });
 
 /**
  * Testing if an object is a true plain object
  */
-test('It should be a javascript plain object', function(t) {
-  t.true(isPlainObj(conf));
-  t.true(isPlainObj(conf.rules));
+test('It should be a javascript plain object', assert => {
+  assert.true(isPlainObj(conf));
+  assert.true(isPlainObj(conf.rules));
 });
