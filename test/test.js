14ae58eb359eac97a6de53368790868817819d75
lbenie <lbenie@coveo.com>
2016-09-13 14:46:44 -0400

    fix(travis): rule missing
    add the missing rule

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ const lint = (str, configuration) => {
 test('It should throw errors', assert => {
   const errors = lint(`'use strict'\nvar foo = function () {};\nfoo();\n`, conf);
   const rules = [
-    'strict', 'semi', 'newline-after-var', 'no-var', 'no-empty-function', 'class-methods-use-this', 'symbol-description', 'prefer-numeric-literals'
+    'strict', 'semi', 'newline-after-var', 'no-var', 'no-empty-function', 'class-methods-use-this', 'symbol-description', 'prefer-numeric-literals', 'line-comment-position', 'lines-around-directive'
   ];
   errors.forEach(error => {
     const id = rules.find(rule => {
