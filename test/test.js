32c7f70600aa1c666e5bd0be961d8e61ec3ae270
Lucien Benie <lbenie@coveo.com>
2016-12-16 13:39:22 -0500

    style(test): change template quote to single quote

--- test/test.js
+++ test/test.js
@@ -24,7 +24,7 @@ const lint = (str, configuration) => {
  * `Eslint` test
  */
 test('It should throw errors', assert => {
-  const errors = lint(`'use strict'\nvar foo = function () {};\nfoo();\n`, conf);
+  const errors = lint('\'use strict\'\nvar foo = function () {};\nfoo();\n', conf);
   const rules = [
     'strict', 'semi', 'newline-after-var', 'no-var', 'no-empty-function', 'class-methods-use-this',
     'symbol-description', 'prefer-numeric-literals', 'line-comment-position',
